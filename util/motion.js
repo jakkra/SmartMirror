7c4ec6b8dda19e770b1f8dd1b6843f06c152e477
Jakob Krantz <ijakkra@gmail.com>
2017-05-27 18:08:20 +0200

    Adds button support

--- util/motion.js
+++ util/motion.js
@@ -1,5 +1,6 @@
 const Gpio = require('onoff').Gpio;
 const pir = new Gpio(18, 'in', 'both');
+const buttron = new Gpio(12, 'in', 'both');
 
 let prevValue = 0;
 module.exports = {
@@ -12,6 +13,13 @@ module.exports = {
 		  if(value === 1 && prevValue === 0) {
 		  	callback();
 		  }
+		});
+		button.watch(function (err, value) {
+		  if (err) {
+		  	console.log('error read button from gpio', err);
+		    throw err;
+		  }
+		  console.log('button change', value);
 		});
 	},
 	exit:function() {
